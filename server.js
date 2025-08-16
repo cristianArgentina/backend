aa43f24cc443de97b26ad2cb5ab31f5b54010934
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2025-08-15 21:42:33 -0300

    cors update

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ import salesRoutes from "./routes/sales.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://cristianargentina.github.io", // tu frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 // Rutas
