7a10db77e3dae84449f69cceb3950231262a3ce7
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2025-08-15 21:55:40 -0300

    trycatch added

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,13 @@ const router = express.Router();
 
 // Obtener todos los productos
 router.get("/", async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    res.status(500).json({ message: "Error al obtener productos", error });
+  }
 });
 
 // Obtener un producto por ID
